33981791917a63449b935e27d9e11a0988d873e8
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-11-04 09:06:18 +0900

    Feat: message controller (#4)

--- utils/io.ts
+++ utils/io.ts
@@ -1,5 +1,6 @@
 import { DefaultEventsMap, Server } from 'socket.io';
 import userController from '../Controllers/user.controller';
+import chatController from '../Controllers/chat.controller';
 
 interface IoProcessProps {
   io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
@@ -14,15 +15,25 @@ const ioProcess = ({ io }: IoProcessProps) => {
     socket.on('login', async (userName, cb) => {
       try {
         const user = await userController.saveUser({ userName, sid: socket.id });
-
+        const welcomeMessage = {
+          chat: `${user.name}가 입장했습니다.`,
+          user: { id: null, name: 'system' },
+        };
+        io.emit('message', welcomeMessage);
         cb({ ok: true, data: user });
       } catch (error) {
         if (error instanceof Error) cb({ ok: false, error: error.message });
       }
     });
     socket.on('sendMessage', async (message, cb) => {
-
-      
+      try {
+        const user = await userController.checkUser({ sid: socket.id });
+        const newMessage = await chatController.saveChat({ message, user });
+        io.emit('message', newMessage);
+        cb({ ok: true });
+      } catch (error) {
+        if (error instanceof Error) cb({ ok: false, error: error.message });
+      }
     });
 
     socket.on('error', (err) => {
